85cf2d0ac142137fbf781bd8e46f0390fc185a29
Phyopyosan <phyoephyoesan786@gmail.com>
2019-12-27 18:02:29 +0630

    first commit

---
+++ Variable.py
@@ -0,0 +1,69 @@
+#27.12.2019
++  _  *  /
+%
+**
+
+ >>> 2 + 2
+ 4
+ >>> 50 - 5 * 6
+ 20
+ >>> (50 -5 * 6) / 4
+ 5.0
+ >>> round(5.0)
+ 5
+ >>> 5.123456
+ 5.123456
+ >>> round(5.123456,2)
+ 5.12
+ >>> 17 / 3
+ 5.6666666667
+ >>> 17 // 3
+ 5
+ >>> 18 // 4
+ 4
+ >>> 20 // 5
+ 4
+ >>> 17 % 3
+ 2
+ >>> 17 * 4 % 3
+ 2
+
+ a = 1
+ a (variable) = (assign) 1 (value)
+
+ weight = 20
+ height = 5 * 9
+ vol = weight * height
+vol
+
+sale = 300000
+tax = 5/100
+total_tax = sale * tax
+total_tax
+total_price = sale + total_tax
+total_price
+
+print('spam eggs')
+print('don\'t')
+print("doesn't")
+print('"Yes"')
+print("\'Yes, \" They said.")
+print('"Isn\'t, " They said')
+
+s = 'First Line.\nSecond Line.'
+print(s)
+
+print("""\
+Usage: thingy
+	    -a
+	    -b
+	    -c
+	""" )
+
+"""...""" or """..."""
+
+3 * 'A'
+2 * 'BC' + 'DE'
+30 * 'GH' + 3
+
+>>> List
